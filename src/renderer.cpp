c2d779231cc0d0fed2a489d623d180970fd85711
Lumi Miettinen <luminaula@gmail.com>
2023-07-28 16:23:44 +0300

    no jthread

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -18,8 +18,29 @@ static int width, height;
 std::vector<ParticleGrid> grids;
 std::list<Lumi::Snow> snow_flakes;
 
-std::unique_ptr<std::jthread> worker_thread;
-std::vector<std::jthread> subworkers;
+class stop_token
+{
+public:
+    stop_token() : stop(std::make_unique<std::atomic_bool>(false))
+    {
+    }
+    bool stop_requested() const noexcept
+    {
+        return *stop;
+    }
+
+    void request_stop() noexcept
+    {
+        *stop = true;
+    }
+private:
+    std::unique_ptr<std::atomic_bool> stop;
+};
+
+std::unique_ptr<std::thread> worker_thread;
+std::unique_ptr<stop_token> worker_stop_token;
+std::vector<std::thread> subworkers;
+std::vector<std::unique_ptr<stop_token>> subworker_stop_tokens;
 
 std::barrier work_barrier(std::thread::hardware_concurrency() + 1);
 std::queue<Rectangle> block_queue;
@@ -27,8 +48,10 @@ std::mutex block_queue_mutex;
 
 using namespace std::literals::chrono_literals;
 
-static void sub_worker(std::stop_token stop_token, int low, int high) {
-    while (!stop_token.stop_requested()) {
+
+
+static void sub_worker(stop_token* stop_token, int low, int high) {
+    while (!stop_token->stop_requested()) {
         work_barrier.arrive_and_wait();
         auto &cur = grids[frame_idx % 2];
         auto &next = grids[(frame_idx + 1) % 2];
@@ -43,15 +66,15 @@ static void sub_worker(std::stop_token stop_token, int low, int high) {
             }
             cur.update_particle_block(next, rect);
         }
-        if (stop_token.stop_requested()) {
+        if (stop_token->stop_requested()) {
             break;
         }
         work_barrier.arrive_and_wait();
     }
 }
 
-static void worker(std::stop_token stop_token) {
-    while (!stop_token.stop_requested()) {
+static void worker(stop_token* stop_token) {
+    while (!stop_token->stop_requested()) {
         frame_idx++;
         std::chrono::time_point start_time = std::chrono::steady_clock::now();
         auto &cur = grids[frame_idx % 2];
@@ -86,8 +109,8 @@ static void worker(std::stop_token stop_token) {
         next.update_snow(snow_flakes);
         std::this_thread::sleep_until(start_time + 16ms);
     }
-    for (auto &thread : subworkers) {
-        thread.request_stop();
+    for (auto &token : subworker_stop_tokens) {
+        token->request_stop();
     }
     work_barrier.arrive_and_drop();
 }
@@ -100,14 +123,16 @@ void init(int width_, int height_) {
     int work_step = height / std::thread::hardware_concurrency() + 1;
     int low = 0;
     for (int i = 0; i < std::thread::hardware_concurrency(); i++) {
-        subworkers.push_back(std::jthread(sub_worker, low, low + work_step));
+        subworker_stop_tokens.emplace_back(std::make_unique<stop_token>());
+        subworkers.push_back(std::thread(sub_worker, subworker_stop_tokens.back().get(), low, low + work_step));
         low += work_step;
     }
-    worker_thread = std::make_unique<std::jthread>(worker);
+    worker_stop_token = std::make_unique<stop_token>();
+    worker_thread = std::make_unique<std::thread>(worker, worker_stop_token.get());
 }
 
 void deinit() {
-    worker_thread->request_stop();
+    worker_stop_token->request_stop();
     worker_thread->join();
 }
 
